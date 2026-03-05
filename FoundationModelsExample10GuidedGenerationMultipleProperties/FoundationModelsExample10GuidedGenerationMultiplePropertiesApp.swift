e14e0b43e82589f2ec4368bc3046d7577fa5308e
samtari1 <samtariyang@gmail.com>
2026-03-05 11:59:19 -0500

    Initial Commit

---
+++ FoundationModelsExample10GuidedGenerationMultipleProperties/FoundationModelsExample10GuidedGenerationMultiplePropertiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoundationModelsExample10GuidedGenerationMultiplePropertiesApp.swift
+//  FoundationModelsExample10GuidedGenerationMultipleProperties
+//
+//  Created by Quanpeng Yang on 3/5/26.
+//
+
+import SwiftUI
+
+@main
+struct FoundationModelsExample10GuidedGenerationMultiplePropertiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FoundationModelsExample10GuidedGenerationMultiplePropertiesTests/FoundationModelsExample10GuidedGenerationMultiplePropertiesTests.swift
@@ -0,0 +1,17 @@
+//
+//  FoundationModelsExample10GuidedGenerationMultiplePropertiesTests.swift
+//  FoundationModelsExample10GuidedGenerationMultiplePropertiesTests
+//
+//  Created by Quanpeng Yang on 3/5/26.
+//
+
+import Testing
+@testable import FoundationModelsExample10GuidedGenerationMultipleProperties
+
+struct FoundationModelsExample10GuidedGenerationMultiplePropertiesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
